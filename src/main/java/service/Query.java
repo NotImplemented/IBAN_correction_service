2bf45f9294f16616e3868f8f6b6622b65f6b6006
NotImplemented <NotImplemented@users.noreply.github.com>
2017-03-02 19:18:37 +0300

    Create Query.java

---
+++ src/main/java/service/Query.java
@@ -0,0 +1,15 @@
+package service;
+
+public class Query {
+
+    private final String text;
+
+    public Query(String text) {
+
+		this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
